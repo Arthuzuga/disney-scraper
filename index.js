ca7dfd708a5e9a54bf79f4314d6060effaf922c2
Joao Tavares <joao.tavares@cubos.io>
2019-05-09 01:59:46 -0300

    feat: add getTicketPrice scraper function

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const getExchangeRate = require('./currency');
+const getTicketPrice = require('./prices');
 
 const url =
 	'https://disneyworld.disney.go.com/pt-br/admission/tickets/theme-parks/';
 
+const numberOfDays = 1;
+
 (async () => {
 	const exchangeRate = await getExchangeRate(url);
 
-	console.log({ exchangeRate });
+	const pricePerPersonPerDay = await getTicketPrice(url, numberOfDays - 1);
+
+	console.log({ exchangeRate, pricePerPersonPerDay });
 	process.exit();
 })();
